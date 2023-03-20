66ed72ad4401fa4192564c76cbaef131b8b2dc42
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2023-03-20 16:17:40 +0530

    Remodel file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
-  theme: {
-    fontFamily: {
-      cursive: ['cursive']
-    },
-    animation: {
-      'spin': "spin 3s linear infinite"
-    },
-    extend: {
-      backgroundImage: {
-        'bgImage': "url('/src/Images/bg.jpg')",
-        'bgImageProject': "url('/src/Images/bg-project.jpg')",
-        'bgImageSkills': "url('/src/Images/bg-skills.jpg')",
-        'bgImageContact': "url('/src/Images/bg-contact.jpg')"
-      }
-    }
-  },
-  plugins: [],
-}
+	content: ['./dist/*.{html,js}'],
+	theme: {
+		fontFamily: {
+			cursive: ['cursive'],
+		},
+		animation: {
+			spin: 'spin 3s linear infinite',
+		},
+		extend: {
+			backgroundImage: {
+				bgImage: "url('/dist/assets/bg.jpg')",
+				bgImageProject: "url('/dist/assets/bg-project.jpg')",
+				bgImageSkills: "url('/dist/assets/bg-skills.jpg')",
+				bgImageContact: "url('/dist/assets/bg-contact.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
